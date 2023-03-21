e7f8acb9e53cffdf4c47e660b5064885af83ceac
boydlakers15 <jboyd115@icloud.com>
2023-03-21 09:06:07 +0100

    Made some changes and added a nav bar

---
+++ hacker-clone/src/Navbar.js
@@ -0,0 +1,22 @@
+import './index.css';
+
+export default function Navbar() {
+    return (
+
+        <nav className="nav">
+      <p className="section">Hacker Clone</p>
+      <ul>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+      </ul>
+
+    </nav>
+    )
+    
+
+}
+
